9c80e389be0f6e36d5b9111d50249009e09fcb5b
shubham <shubhamh131@gmail.com>
2024-02-17 13:00:45 +0530

    added src code

---
+++ 01_variables.js
@@ -0,0 +1,22 @@
+// const accountId = 1234
+// let accountEmail = "shubham@gmail.com"
+// var accountPassword = "4567"
+// accountCity = "solapur"
+
+// accountEmail = "sh@sh.com"
+// accountPassword = "121212121"
+// accountCity = "pune"
+
+
+
+//console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+const obj= {
+
+
+  accountId: 1234,
+  accountEmail: "shubh@gmail.com",
+  accountPassword: "shub123"
+
+}
+console.log(obj);
